b8f3825517961ec099add55a17acce75befc2253
anamika7015 <149082695+anamika7015@users.noreply.github.com>
2024-04-17 22:27:28 +0530

    This is my first commit

--- 03_lecture/third.js
+++ 03_lecture/third.js
@@ -1,1 +1,6 @@
-console.log("My first javascript code");
+console.log(`hello`);
+console.log(`I like pizza`);
+window.alert(`this is an alert`);
+document.getElementById("myp").textContent = `i like pizza`;
+
+document.getElementsById("myH1").textcontent = `Hello`;
