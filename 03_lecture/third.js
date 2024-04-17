2d09e3a1224ee7b2ddb52b5e800218e9af0ec80d
anamika7015 <149082695+anamika7015@users.noreply.github.com>
2024-04-17 22:47:44 +0530

    update third js

--- 03_lecture/third.js
+++ 03_lecture/third.js
@@ -4,3 +4,26 @@ window.alert(`this is an alert`);
 document.getElementById("myp").textContent = `i like pizza`;
 
 document.getElementsById("myH1").textcontent = `Hello`;
+
+// variables = A container that stores a value.
+     //        Behaves as if it were the value it contains
+
+     //decleration let x;
+     // 2 assignment x =100;
+
+
+
+     let x = 100;
+     console.log(x);
+
+     let age =18;
+     let prices = 100;
+     let gpa = 2.1;
+     console.log(`you are ${age} years old`);
+     console.log(`this price is ${ prices}`);
+
+     let firstName = "Anamika";
+     let favoritefood = "pizza";
+
+     console.log(typeof firstName);
+     console.log(`my nsme is ${firstName}`);
