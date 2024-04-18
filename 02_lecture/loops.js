a12778e7e4d3070cd4a5253b8a751c42335cd8be
anamika7015 <149082695+anamika7015@users.noreply.github.com>
2024-04-19 02:56:18 +0530

    for of loop

--- 02_lecture/loops.js
+++ 02_lecture/loops.js
@@ -11,7 +11,7 @@ for(let i=0;i<=10;i++){
 
 let index =0
 while(index<=10){
-    console.log(`value of index is ${index}`);
+    // console.log(`value of index is ${index}`);
     index = index+2
 }
 
@@ -19,15 +19,42 @@ while(index<=10){
 let myarray =["flash","batsman","superman"]
 let arr =0;
 while(arr<myarray.length){
-console.log(`value is ${myarray[arr]}`);
+// console.log(`value is ${myarray[arr]}`);
 arr = arr+1
 }
 
 
 //                          DO WHILE LOOPS 
 let score =11
-do{
-    console.log(`scoreis${score}`);
+ do{
+//     console.log(`scoreis${score}`);
     score++
 }while(score<=10)
+
+//           FOR OF
+
+const array = [1,2,3,4,5]
+for (const num of array) {
+    console.log(num);
+    
+}
+
+const greetings = "hello world!"
+for (const greet of greetings) {
+    console.log(greet);
+}
+ 
+
+//           maps = unique values
+const map = new Map()
+map.set('In',"India")
+map.set('USA',"United state of America")
+map.set('Fr',"France")
+map.set('In',"India")
+console.log(map);
+for (const [key,value] of map) {
+    console.log(key,':-' ,value);
+
+}
+
 
