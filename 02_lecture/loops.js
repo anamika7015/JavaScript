d6c6221130017aa30a491c15f432d8180b9c5639
anamika7015 <149082695+anamika7015@users.noreply.github.com>
2024-04-18 21:14:12 +0530

    while loop

--- 02_lecture/loops.js
+++ 02_lecture/loops.js
@@ -1,7 +1,24 @@
 for(let i=0;i<=10;i++){
     const element =i;
     if(element==5){
-        console.log("5 is best number");
+        // console.log("5 is best number");
     }
-    console.log(element);
+    // console.log(element);
+}
+
+
+//            while
+
+let index =0
+while(index<=10){
+    console.log(`value of index is ${index}`);
+    index = index+2
+}
+
+
+let myarray =["flash","batsman","superman"]
+let arr =0;
+while(arr<myarray.length){
+console.log(`value is ${myarray[arr]}`);
+arr = arr+1
 }
