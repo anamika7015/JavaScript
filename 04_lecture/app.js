9fc50089fc6c44e717c5d32bbc167a89b2e42f5a
anamika7015 <149082695+anamika7015@users.noreply.github.com>
2024-04-19 02:30:29 +0530

    arithmetic operators

---
+++ 04_lecture/app.js
@@ -0,0 +1,45 @@
+let fullName = "Anamika pandey";
+let age = 18;
+// let student = true;
+
+
+
+
+
+document.getElementById("p1").textContent = `My name is ${fullName}`;
+document.getElementById("p2").textContent = age;
+// document.getElementById("p3").textContent = isStudent;
+
+
+
+//                                        ARITHMATIC OPERATORS = OPERANDS(value,variable,etc)
+
+let student = 30;
+// student = student+19;
+// student = student-1;
+// student = student*2;
+// student = student/2;
+//  student = student**2;
+//  student = student%2;
+
+// student += 1;
+// student -= 5;
+// student *= 5;
+// student /= 5;
+// student **= 5;
+// student %= 5;
+// student++;
+student--;
+
+
+console.log(student);
+
+/*
+operator precedance
+1. paranthesis()
+2. exponents
+3. multiplication & division & modulus
+4. Addition & subtraction
+ */
+let result = 1+2*3+4**2;
+console.log(result);
