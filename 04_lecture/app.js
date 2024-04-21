6188f95a70489a8438ec5bcabe627f8fe4a7681f
anamika7015 <149082695+anamika7015@users.noreply.github.com>
2024-04-21 11:17:54 +0530

    type conversion

--- 04_lecture/app.js
+++ 04_lecture/app.js
@@ -1,20 +1,20 @@
-let fullName = "Anamika pandey";
-let age = 18;
+// let fullName = "Anamika pandey";
+// let age = 18;
 // let student = true;
 
 
 
 
 
-document.getElementById("p1").textContent = `My name is ${fullName}`;
-document.getElementById("p2").textContent = age;
+// document.getElementById("p1").textContent = `My name is ${fullName}`;
+// document.getElementById("p2").textContent = age;
 // document.getElementById("p3").textContent = isStudent;
 
 
 
 //                                        ARITHMATIC OPERATORS = OPERANDS(value,variable,etc)
 
-let student = 30;
+// let student = 30;
 // student = student+19;
 // student = student-1;
 // student = student*2;
@@ -29,17 +29,43 @@ let student = 30;
 // student **= 5;
 // student %= 5;
 // student++;
-student--;
+// student--;
 
 
-console.log(student);
+// console.log(student);
 
 /*
 operator precedance
 1. paranthesis()
 2. exponents
 3. multiplication & division & modulus
-4. Addition & subtraction
- */
+4. Addition & subtract
 let result = 1+2*3+4**2;
 console.log(result);
+
+// TYPE CONVERSION = CHANGE the datatype of value to another
+(string,numbers,booleans)*/
+
+// let age = window.prompt(" How old are you");
+// age = Number(age);
+//  age+=1;
+//  console.log(age ,typeof age);
+  
+
+ let x = "0";
+ let y = "0";
+ let z = "0";
+
+
+ x = Number(x);
+ y = String(y);
+ z = Boolean(z);
+//  let a = "0";
+//  let b = "0";
+//  let c = "0";
+ console.log(x,typeof x);
+ console.log(y, typeof y);
+ console.log(z, typeof z);
+//  console.log(a, typeof a);
+//  console.log(b, typeof b);
+//  console.log(c, typeof c);
