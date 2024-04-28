499949a6a4a03f443904c3ffc5bea6ee236614cb
anamika7015 <149082695+anamika7015@users.noreply.github.com>
2024-04-28 13:48:23 +0530

    if else or else if statement

---
+++ 10_lecture/app.js
@@ -0,0 +1,32 @@
+// IF STATEMENT = if a condition is true ,execute some code if
+// not, do something else
+
+// // let age =18;
+// // let hasLicence = true;
+// // if(age>=16){
+//     // console.log("you are old enough to derive");
+//     if(hasLicence){
+//         // console.log("you have your licence");
+//     }
+//     else{
+//         // console.log("you do not the licence yet!");
+//     }
+// }
+// else{
+//     // console.log("you must be 16+ to have the driving licence");
+// }
+
+//////////////////////////////////////////////ELSEIF//////////////
+
+let age = 102;
+
+ if (age >= 101) {
+    console.log(" you are  too old to enough to  this site");
+ }
+else if (age >= 18) {
+  console.log("you are able to enter this site");
+} else if (age < 0) {
+  console.log("your age can't be below 0");
+} else {
+    console.log("you are not able to enter this site");
+}
