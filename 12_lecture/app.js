d4f203565ad2292fc500500d46ba529576cf67fb
anamika7015 <149082695+anamika7015@users.noreply.github.com>
2024-04-28 17:39:46 +0530

    Ternary operators

---
+++ 12_lecture/app.js
@@ -0,0 +1,3 @@
+// ternary operator = a shortcut to if{} and else{} statements
+//                   helps to assign a variable based on condition
+                    //   conditions ? codeifTrue:  codeifFalse
