da103eb026b34819a65845b8eb491e88f57bbd3a
anamika7015 <149082695+anamika7015@users.noreply.github.com>
2024-04-28 17:52:09 +0530

    use of ternary operators

--- 12_lecture/app.js
+++ 12_lecture/app.js
@@ -1,3 +1,16 @@
 // ternary operator = a shortcut to if{} and else{} statements
 //                   helps to assign a variable based on condition
-                    //   conditions ? codeifTrue:  codeifFalse
+                    //   conditions ? codeifTrue:  codeifFals
+
+// let age =12;
+// let message = age>=18 ? "you are adult" : "you are not adult"
+// console.log(message);
+
+
+// let isStudent = false;
+// let message = isStudent ? "you are a student" : "you are not student"
+// console.log(message);
+
+let time = 9;
+let greetings = time<12 ? "good morning" : "good afternoon"
+console.log(greetings);
