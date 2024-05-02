c5c2ac381de97cd31cf4dc8ce782be0ebb2468d6
anamika7015 <149082695+anamika7015@users.noreply.github.com>
2024-05-02 17:46:09 +0530

    assignment operator

---
+++ 18_lecture/app.js
@@ -0,0 +1,13 @@
+// = assignment aperators
+// == comparision operators
+// === strict equality operators (compare if values & datatype are equal)
+// != inequality operators
+// !== strict inequality operators
+
+const PI = 3.14;
+if(PI !== "3.14"){
+    console.log("that is not pi");
+}
+else{
+    console.log("that is pi");
+}
