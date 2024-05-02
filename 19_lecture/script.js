e7a06a9702f162899bff7411a07a0546f000955e
anamika7015 <149082695+anamika7015@users.noreply.github.com>
2024-05-02 20:24:07 +0530

    using while loop in js

--- 19_lecture/script.js
+++ 19_lecture/script.js
@@ -1,19 +1,21 @@
 // while loop = repeat some code while some condition is true
 
-let loggedin;
+let loggedin = false;
 let username;
 let password;
-while(loggedin) {
+while(!loggedin) {
     username = window.prompt("enter the user name");
     password = window.prompt("enter the passward");
     
     
     if(username === "myusername" && password === "myPassword"){
-        loggedin = true;
-        console.log("you are logged in");
-
+      loggedin = true;
+        
+        
+        window.alert("you are logged in");
     }
     else{
-        console.log("you are not logged in");
+        window.alert("you are not logged in");
+      
     }
 }
