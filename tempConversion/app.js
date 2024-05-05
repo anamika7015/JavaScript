bb890e410b23520db10a31cd7c8d8dd7819afb55
anamika7015 <149082695+anamika7015@users.noreply.github.com>
2024-05-06 05:25:54 +0530

    update

---
+++ tempConversion/app.js
@@ -0,0 +1,5 @@
+// Temperature conversion Program
+const textBox = document.getElementById("textBox");
+const toFahrenheit =document.getElementById("toFahrenheit");
+const toCelsius =document.getElementById("toCelsius");
+const myButton =document.getElementById("myButton");
