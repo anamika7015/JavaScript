da4960dbbd10b887ac6b7ec3084f4c3e5206870f
anamika7015 <149082695+anamika7015@users.noreply.github.com>
2024-05-06 06:05:54 +0530

    temperature converter

--- tempConversion/app.js
+++ tempConversion/app.js
@@ -2,4 +2,23 @@
 const textBox = document.getElementById("textBox");
 const toFahrenheit =document.getElementById("toFahrenheit");
 const toCelsius =document.getElementById("toCelsius");
-const myButton =document.getElementById("myButton");
+const result =document.getElementById("result");
+let temp;
+
+function convert(){
+    if(toFahrenheit.checked){
+        temp = Number(textBox.value);
+        temp = temp * 9 / 5 + 32;
+        result.textContent = temp.toFixed() + "°F";
+    }
+    else if(toCelsius.checked){
+        temp = Number(textBox.value);
+        temp = (temp - 32) *(5/9);
+        result.textContent = temp.toFixed() + "°C";
+    }
+
+    
+    else{
+        result.textContent = "select a unit";
+    }
+}
