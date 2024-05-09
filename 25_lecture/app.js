21c18445a70a9c9e9a59de54d0d9d0c3f902e239
anamika7015 <149082695+anamika7015@users.noreply.github.com>
2024-05-09 20:03:39 +0530

    forEach function in js

--- 25_lecture/app.js
+++ 25_lecture/app.js
@@ -1,1 +1,27 @@
-// forEach() = method used to iterate over a function 
+// forEach() = method used to iterate over the elements 
+//            in array and apply a spacified function (callback)
+            // to each elements
+            //   array.forEach(callback)
+            // elements , array, index are provided
+
+            let fruits =["apple","banana","orange","coconut"];
+            
+            fruits.forEach(capatlize)
+            fruits.forEach(display)
+            function uppercase(element, index,array) {
+                array[index] = element.toUpperCase();
+            }
+          function lowercase(element, index,array) {
+            array[index] = element.toLowerCase();
+          }
+
+          function capatlize(element, index,array) {
+            array[index] = element.charAt(0).toUpperCase()+element.slice(1);
+          }
+
+
+
+
+            function display(elements) {
+            console.log(elements);
+            }
