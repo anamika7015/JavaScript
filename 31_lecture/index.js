ae5b0f16e8cde47d11cedf2b51c360475d14ba81
anamika7015 <149082695+anamika7015@users.noreply.github.com>
2024-05-10 13:33:05 +0530

    objects

---
+++ 31_lecture/index.js
@@ -0,0 +1,5 @@
+// objects = a collection of related properties and/ or methods
+//  can represent real world objects (people, products, places)
+//    objects = {key: value,
+//               function()}
+
