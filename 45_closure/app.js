53e35a095a4343c1a2794023af6f2fb25ced874a
anamika7015 <149082695+anamika7015@users.noreply.github.com>
2024-05-12 22:27:29 +0530

    closure in js

--- 45_closure/app.js
+++ 45_closure/app.js
@@ -4,4 +4,46 @@
 //  Allow for private variables  and state maintenance
 //  used frequently in js framework : React ,Vue,Anguler
 
+function createCount() {
+    let count = 0;
+    function increment() { 
+        count++;
+        console.log(`count increased ${count}`);
+        
+    }
+    function getcount() {
+        return count;
+        
+    }
+    return {increment,getcount};
+    
+}
+ const counter  = createCount();
+counter.increment();
+counter.increment();
+counter.increment();
+counter.increment();
+counter.increment();
+
+console.log(`the current count is ${counter.getcount()}`);
+
+// example 2
+function createGame() {
+    
+    let score = 0;
+    function increaseScore(points) {
+        score += points;
+        console.log(`+${points}pts`);
+        
+    }
+    function decreaseScore(points) {
+        score -= points;
+        console.log(`-${points}pts`);
+        
+    }
+    return {increaseScore,decreaseScore}
+}
+const game = createGame();
+game.increaseScore(3);
+game.decreaseScore(2);
 
