b19549e00bdde16cc5ed9bf4224007e76e2c3617
anamika7015 <149082695+anamika7015@users.noreply.github.com>
2024-05-12 22:01:15 +0530

    closure in js

---
+++ 45_closure/app.js
@@ -0,0 +1,7 @@
+//  closure = a function  defined inside of another function,
+// the inner function has access to the variables 
+// and scope of the outer function.
+//  Allow for private variables  and state maintenance
+//  used frequently in js framework : React ,Vue,Anguler
+
+
