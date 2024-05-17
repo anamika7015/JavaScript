c21b398dff6a37fe702b9d5460c9a556d9c6df98
anamika7015 <149082695+anamika7015@users.noreply.github.com>
2024-05-17 21:47:26 +0530

    DOM Manpulation

---
+++ 52_Dom/app.js
@@ -0,0 +1,7 @@
+// DOM = DOCUMENT OBJECT MODEL
+//     object{} that represent the page you see in the web browser
+//    and provides you with an API  to interact with it.
+//   web browser construct the DOM when it loads an html documents,
+//   and structure all the element in a tree - like representation 
+//   javascript can access the dom to dynamically
+//   change the content ,structure, and style of a web page
