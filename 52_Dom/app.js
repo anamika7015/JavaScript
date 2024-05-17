3b3664914e2253e3e984032ab54c12e0e3fe6ad6
anamika7015 <149082695+anamika7015@users.noreply.github.com>
2024-05-17 21:59:59 +0530

    update

--- 52_Dom/app.js
+++ 52_Dom/app.js
@@ -5,3 +5,9 @@
 //   and structure all the element in a tree - like representation 
 //   javascript can access the dom to dynamically
 //   change the content ,structure, and style of a web page
+
+const username = "Brocode";
+
+const welcomemsg = document.getElementById("welcome-msg");
+welcomemsg.textContent += username === ""? `guest` : username;
+console.dir(document);
