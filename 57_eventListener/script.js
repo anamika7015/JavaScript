4ad14d916c6d3e8c8f5e36300a59c884d6fd1bf3
anamika7015 <149082695+anamika7015@users.noreply.github.com>
2024-05-20 21:54:35 +0530

    eventlistner for keydown and keyup

--- 57_eventListener/script.js
+++ 57_eventListener/script.js
@@ -1,3 +1,46 @@
 // eventListner = Listen for specific events to create interactive web pages
 //                events: click,mouseover, mouseout
-//                .addEventListener(event, callback);
+//                .addEventListener(event, callback/anonmus function/arrow function);
+
+// const mybox = document.getElementById("mybox");
+// const mybtn = document.getElementById("mybtn");
+
+// mybox.addEventListener("click",event => {
+//     event.target.style.backgroundColor = "tomato";
+//     event.target.textContent = "OUCH ! 🤕";
+
+    
+// });
+
+// mybox.addEventListener("mouseover",event =>{
+//     event.target.style.backgroundColor = "yellow";
+//     event.target.textContent = "Don't do it 😮 ";
+// });
+
+// mybox.addEventListener("mouseout",event =>{
+//     event.target.style.backgroundColor = "lightblue";
+//     event.target.textContent = " Click me 😊  ";
+// });
+
+// ------------------button event-----------
+
+const mybox = document.getElementById("mybox");
+const mybtn = document.getElementById("mybtn");
+
+mybtn.addEventListener("click",event => {
+    mybox.style.backgroundColor = "tomato";
+    mybox.textContent = "OUCH ! 🤕";
+
+    
+});
+
+mybtn.addEventListener("mouseover",event =>{
+    mybox.style.backgroundColor = "yellow";
+    mybox.textContent = "Don't do it 😮 ";
+});
+
+mybtn.addEventListener("mouseout",event =>{
+    mybox.style.backgroundColor = "lightblue";
+    mybox.textContent = " Click me 😊  ";
+});
+
---
+++ 58_eventListener2/app.js
@@ -0,0 +1,7 @@
+// eventListner = Listen for specific events to create interactive web pages
+//               events: keydown, keyup
+//              document.addEventListener(event, callback);
+
+document.addEventListener("keydown",event =>{
+    
+})
