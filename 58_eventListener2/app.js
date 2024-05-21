95a2dff2c0bcd0ce9cdb2ef82e548c3738b6532f
anamika7015 <149082695+anamika7015@users.noreply.github.com>
2024-05-21 22:28:20 +0530

    event listener

--- 58_eventListener2/app.js
+++ 58_eventListener2/app.js
@@ -3,16 +3,49 @@
 //              document.addEventListener(event, callback);
 
 const mybox = document.getElementById("mybox");
-
+const moveAmount = 100;
+let x= 0;
+let y = 0;
 
 
 document.addEventListener("keydown",event =>{
     mybox.textContent = "😮";
     mybox.style.backgroundColor ="tomato";
 });
-
 document.addEventListener("keyup",event =>{
     mybox.textContent = "😎";
     mybox.style.backgroundColor ="lightblue";
 });
+
+
+    document.addEventListener("keydown", event =>{
+        if(event.key.startsWith("Arrow")) {
+            
+        
+            event.preventDefault();
+    
+            switch(event.key){
+                case  "ArrowUp":
+                    y -= moveAmount;
+                    break;
+                case "ArrowDown":
+                    y += moveAmount;
+                    break;
+    
+                case "ArrowLeft":
+                    x -= moveAmount;
+                    break;
+                case "ArrowRight":
+                    x += moveAmount;
+                    break;
+            }
+            mybox.style.top = `${y}px`;
+            mybox.style.left = `${x}px`;
+        }
+    });
+
+    
+
+
+
 
