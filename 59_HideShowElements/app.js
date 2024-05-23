3ad8ece59f80f495be23a437986bf7285305ba97
anamika7015 <149082695+anamika7015@users.noreply.github.com>
2024-05-23 15:38:18 +0530

    i update your show and hide file

--- 59_HideShowElements/app.js
+++ 59_HideShowElements/app.js
@@ -1,13 +1,18 @@
 const mybtn = document.getElementById("mybtn");
 const  myImg = document.getElementById("myImg");
+const bgcolor = document.getElementById("container");
 
 mybtn.addEventListener("click", event =>{
     if(myImg.style.visibility === "hidden"){
         myImg.style.visibility = "visible";
         mybtn.textContent = "Hide";
+        message.textContent = ""
+        bgcolor.style.backgroundColor = "#fff"
     }
     else{
         myImg.style.visibility = "hidden";
         mybtn.textContent = "Show";
+        message.textContent = "Click here to show the image"
+        bgcolor.style.backgroundColor = "chocolate"
     }
 });
