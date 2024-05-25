30fd1d728a109f662d24ad98690ee57c3c9346f9
anamika7015 <149082695+anamika7015@users.noreply.github.com>
2024-05-25 15:24:53 +0530

    update

--- 58_eventListener2/app.js
+++ 58_eventListener2/app.js
@@ -4,48 +4,38 @@
 
 const mybox = document.getElementById("mybox");
 const moveAmount = 100;
-let x= 0;
+let x = 0;
 let y = 0;
 
-
-document.addEventListener("keydown",event =>{
-    mybox.textContent = "😮";
-    mybox.style.backgroundColor ="tomato";
+document.addEventListener("keydown", event => {
+  mybox.textContent = "😮";
+  mybox.style.backgroundColor = "tomato";
 });
-document.addEventListener("keyup",event =>{
-    mybox.textContent = "😎";
-    mybox.style.backgroundColor ="lightblue";
+document.addEventListener("keyup", event => {
+  mybox.textContent = "😎";
+  mybox.style.backgroundColor = "lightblue";
 });
 
+document.addEventListener("keydown", event => {
+  if (event.key.startsWith("Arrow")) {
+    event.preventDefault();
 
-    document.addEventListener("keydown", event =>{
-        if(event.key.startsWith("Arrow")) {
-            
-        
-            event.preventDefault();
-    
-            switch(event.key){
-                case  "ArrowUp":
-                    y -= moveAmount;
-                    break;
-                case "ArrowDown":
-                    y += moveAmount;
-                    break;
-    
-                case "ArrowLeft":
-                    x -= moveAmount;
-                    break;
-                case "ArrowRight":
-                    x += moveAmount;
-                    break;
-            }
-            mybox.style.top = `${y}px`;
-            mybox.style.left = `${x}px`;
-        }
-    });
+    switch (event.key) {
+      case "ArrowUp":
+        y -= moveAmount;
+        break;
+      case "ArrowDown":
+        y += moveAmount;
+        break;
 
-    
-
-
-
-
+      case "ArrowLeft":
+        x -= moveAmount;
+        break;
+      case "ArrowRight":
+        x += moveAmount;
+        break;
+    }
+    mybox.style.top = `${y}px`;
+    mybox.style.left = `${x}px`;
+  }
+});
