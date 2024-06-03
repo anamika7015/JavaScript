e13a42ec098e700cd5c9b0481d6b6f53fdc6fe7d
anamika7015 <149082695+anamika7015@users.noreply.github.com>
2024-06-04 04:46:54 +0530

    image slider

---
+++ 62_imageSlider/app.js
@@ -0,0 +1,18 @@
+// IMAGE SLIDER 
+
+const slides = document.querySelectorAll(".slides img");
+let slideIndex = 0;
+let intervalId = null;
+function initializeSlider() {
+ slides[slideIndex].classList.add("displaySlide");
+    
+}
+function showslide() {
+    
+}
+function prevslide(params) {
+    
+}
+function nextslide(params) {
+    
+}
